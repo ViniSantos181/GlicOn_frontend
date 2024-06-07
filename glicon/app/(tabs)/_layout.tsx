8283e79362c2d348d4355b3f7ddc046b89aa5889
Vinícius Santos <vinisantos181@hotmail.com>
2024-06-07 15:30:25 -0300

    feat:

--- glicon/app/(tabs)/_layout.tsx
+++ glicon/app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Buscar',
+          title: 'Home',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
@@ -26,7 +26,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Agenda',
+          title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
